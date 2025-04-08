7bbbcfc2c5fb1b57aa153701a6212799ce69cd36
Şilan Pehlivan <161645151+SilanPehlivan9X13E@users.noreply.github.com>
2025-04-08 15:07:50 +0300

    Add project files.

---
+++ ETicaretProjesi/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using ETicaretProjesi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretProjesi.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 25000, ImageUrl = "\"C:\\Users\\HP\\Pictures\\laptop.jpg\"", Description = "Güçlü bir laptop" },
+            new Product { Id = 2, Name = "Telefon", Price = 14000, ImageUrl = "\"C:\\Users\\HP\\Pictures\\telefon.jpg\"", Description = "Akıllı telefon" }
+        };
+
+            return View(products);
+
+        }
+    }
+}
---
+++ ETicaretProjesi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ETicaretProjesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
